d8192548ee54883b37e440736ba8c55e79254b3c
Leoces95 <leocesvives@gmail.com>
2026-08-29 16:31:08 -0500

    Agrega Controller Status primer endpoint

---
+++ TeamGitPractice/Controllers/StatusController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeamGitPractice.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StatusController : ControllerBase  
+    {
+        [HttpGet]
+        public IActionResult GetStatus()
+        {
+            return Ok(new { status = "API is running" });
+        }
+    }
+}
