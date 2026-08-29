8530a8b0d41f736aeb021bec21d849858e8c0bfc
Daniel Zapata <42874951+DanielZR30@users.noreply.github.com>
2026-08-29 16:35:24 -0500

    feat: add GetMembers endpoint

---
+++ TeamGitPractice/Controllers/MembersController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeamGitPractice.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MembersController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetMembers()
+    {
+        var members = new[]
+        {
+            "María Paulina Vargas Lenis",
+            "Mariana Suaza Serna",
+            "Leonel Antonio Martínez silgado",
+            "Sebastián Ciro Medellín",
+            "Daniel Zapata Ramírez"
+        };
+
+        return Ok(members);
+    }
+}
