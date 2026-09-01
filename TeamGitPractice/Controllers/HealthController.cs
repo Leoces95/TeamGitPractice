60f46c72e600d33a68b6efa80f43b347bc4f2083
Mariana Suaza Serna <marianasuaza15@gmail.com>
2026-08-31 22:50:19 -0500

    feat: extend health endpoint

--- TeamGitPractice/Controllers/HealthController.cs
+++ TeamGitPractice/Controllers/HealthController.cs
@@ -10,4 +10,10 @@ public class HealthController : ControllerBase
     {
         return Ok(new { healthy = true });
     }
+
+    [HttpGet("time")]
+    public IActionResult GetTime()
+    {
+        return Ok(new { utcTime = DateTime.UtcNow });
+    }
 }
